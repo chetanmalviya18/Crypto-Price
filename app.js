15fcdea79cb941c06e1024555e65a9b66c5e1a0f
chetanmalviya18 <chetanmalviya.182@gmail.com>
2024-01-17 10:27:57 +0530

    Project Complete

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 import express from "express";
 import axios from "axios";
-import ejs from "ejs";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const cryptocurrencies = [
-    { name: 'Bitcoin', symbol: 'BTC', price: 45000 },
-    { name: 'Ethereum', symbol: 'ETH', price: 3000 },
-    // Add more cryptocurrencies as needed
-];
+app.get('/', (req, res) =>{
+    res.render("crypto.ejs", 
+    { symbol: "xxx-xxx",
+price: "xxxx"});
+})
 
-
-
-app.get('/', (req, res) => {
-    res.render('crypto', { cryptocurrencies });
+app.post('/', async (req, res) => {
+    const bar = req.body.searchbar;
+    try {
+        const result = await axios.get(`https://api.blockchain.com/v3/exchange/tickers/${bar}`)
+        res.render("crypto.ejs", 
+        { symbols: result.data.symbol,
+        prices: result.data.price_24h});
+    } catch (error) {
+        res.status(500);
+    }
 });
 
 app.listen(port, () => {
